8247fe820feeb1e2247b1ad06fd6bbae0d14b9b7
Raul Belmonte de Souza <raulbelmontedesouza@gmail.com>
2022-01-30 21:07:37 -0300

    This is a piece of code that separete the photos that are in my phone backups.

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+    public static List<File> files = new ArrayList<>();
+    public static Set<String> years = new HashSet<>();
+
+    public static void findAllFilesInFolder(File folder) {
+        for (File file: folder.listFiles()) {
+            if (!file.isDirectory()) {
+                files.add(file);
+                years.add(readYear(file));
+            } else {
+                findAllFilesInFolder(file);
+            }
+        }
+    }
+
+    public static String readYear(File file) {
+        String year = "";
+        if (file.getName().startsWith("IMG-") || file.getName().startsWith("VID-")) {
+            year = file.getName().substring(4, 8);
+        } else if (file.getName().startsWith("FB_IMG") || file.getName().startsWith("Screen")) {
+            return "others";
+        } else {
+            year = file.getName().substring(0, 4);
+        }
+        return year;
+
+    }
+
+    public static String readMonth(File file) {
+        String month = "";
+        if (file.getName().startsWith("IMG-") || file.getName().startsWith("VID-")) {
+            month = file.getName().substring(8, 10);
+        } else if (file.getName().startsWith("FB_IMG") || file.getName().startsWith("Screen")) {
+            return "others";
+        } else {
+            month = file.getName().substring(4, 6);
+        }
+        return month;
+
+    }
+
+    private static void moveFiles(List<File> files) throws IOException {
+        for (File file : files) {
+            String target = "C:\\+originPath+\\" + readYear(file) + "\\" + readMonth(file) + "\\" + file.getName();
+            File olfFile = new File(file.getAbsolutePath());
+            File newFile = new File(target);
+            newFile.mkdirs();
+
+
+            Files.move(olfFile.toPath(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String originPath = "/teste";
+        File folder = new File(originPath);
+
+        findAllFilesInFolder(folder);
+        moveFiles(files);
+    }
+
+
+}
